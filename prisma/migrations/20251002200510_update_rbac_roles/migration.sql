5bf23aa1197b8e250d462fe1a535e4b6db409070
adityarove <aditya.jaif@gorove.in>
2025-10-10 16:44:54 +0530

    Fix: Simplify enum migration to use EXECUTE and avoid type reference issues

--- prisma/migrations/20251002200510_update_rbac_roles/migration.sql
+++ prisma/migrations/20251002200510_update_rbac_roles/migration.sql
@@ -1,24 +1,26 @@
 -- Update RoleName enum to include new role types
--- Split into steps to avoid PostgreSQL enum constraint issues
+-- Simplified migration that just adds values, ignoring duplicates
 
--- Step 1: Add the new enum values (if not already added)
+-- Add GENERAL_MANAGER if it doesn't exist
 DO $$ 
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = 'GENERAL_MANAGER' AND enumtypid = 'RoleName'::regtype) THEN
-    ALTER TYPE "RoleName" ADD VALUE 'GENERAL_MANAGER';
+  IF NOT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = 'GENERAL_MANAGER') THEN
+    EXECUTE 'ALTER TYPE "RoleName" ADD VALUE ''GENERAL_MANAGER''';
   END IF;
 END $$;
 
+-- Add SALES_MANAGER if it doesn't exist
 DO $$ 
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = 'SALES_MANAGER' AND enumtypid = 'RoleName'::regtype) THEN
-    ALTER TYPE "RoleName" ADD VALUE 'SALES_MANAGER';
+  IF NOT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = 'SALES_MANAGER') THEN
+    EXECUTE 'ALTER TYPE "RoleName" ADD VALUE ''SALES_MANAGER''';
   END IF;
 END $$;
 
+-- Add CUSTOMER_ADVISOR if it doesn't exist
 DO $$ 
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = 'CUSTOMER_ADVISOR' AND enumtypid = 'RoleName'::regtype) THEN
-    ALTER TYPE "RoleName" ADD VALUE 'CUSTOMER_ADVISOR';
+  IF NOT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = 'CUSTOMER_ADVISOR') THEN
+    EXECUTE 'ALTER TYPE "RoleName" ADD VALUE ''CUSTOMER_ADVISOR''';
   END IF;
 END $$;
